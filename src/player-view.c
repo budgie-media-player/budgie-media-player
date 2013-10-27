c7b8690001cec4ccf5dfaa64a602328357765676
Ikey Doherty <ikey.doherty@gmail.com>
2013-10-27 22:36:03 +0000

    Ellipsize genre column, because its not that interesting.

--- src/player-view.c
+++ src/player-view.c
@@ -90,6 +90,7 @@ static void player_view_init(PlayerView *self)
         cell_text = gtk_cell_renderer_text_new();
         column = gtk_tree_view_column_new_with_attributes("Genre",
                 cell_text, "text", PLAYER_COLUMN_GENRE);
+        g_object_set(cell_text, "ellipsize", PANGO_ELLIPSIZE_END, NULL);
         gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
         gtk_tree_view_column_set_resizable(column, TRUE);
         gtk_tree_view_column_set_sort_column_id(column, PLAYER_COLUMN_GENRE);
