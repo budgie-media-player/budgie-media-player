ef47dc71b59c9a6552d0674ce5814835d1f68d6d
Ikey Doherty <ikey.doherty@gmail.com>
2013-10-27 15:45:03 +0000

    Add skeleton for PlayerView
    PlayerView will essentially hold the current playlist for tracks, etc.

---
+++ src/player-view.c
@@ -0,0 +1,58 @@
+/*
+ * player-view.c
+ * 
+ * Copyright 2013 Ikey Doherty <ikey.doherty@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+#include "player-view.h"
+
+G_DEFINE_TYPE_WITH_PRIVATE(PlayerView, player_view, GTK_TYPE_BIN);
+
+/* Initialisation */
+static void player_view_class_init(PlayerViewClass *klass)
+{
+        GObjectClass *g_object_class;
+
+        g_object_class = G_OBJECT_CLASS(klass);
+        g_object_class->dispose = &player_view_dispose;
+}
+
+static void player_view_init(PlayerView *self)
+{
+        /* Not yet implemented */
+}
+
+static void player_view_dispose(GObject *object)
+{
+        PlayerView *self;
+
+        self = PLAYER_VIEW(object);
+
+        /* Destruct */
+        G_OBJECT_CLASS (player_view_parent_class)->dispose (object);
+}
+
+/* Utility; return a new PlayerView */
+GtkWidget* player_view_new(void)
+{
+        PlayerView *self;
+
+        self = g_object_new(PLAYER_VIEW_TYPE, NULL);
+        return GTK_WIDGET(self);
+}
---
+++ src/player-view.h
@@ -0,0 +1,67 @@
+/*
+ * player-view.h
+ * 
+ * Copyright 2013 Ikey Doherty <ikey.doherty@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+#ifndef player_view_h
+#define player_view_h
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+typedef struct _PlayerView PlayerView;
+typedef struct _PlayerViewClass   PlayerViewClass;
+typedef struct _PlayerViewPrivate PlayerViewPrivate;
+
+#define PLAYER_VIEW_TYPE (player_view_get_type())
+#define PLAYER_VIEW(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), PLAYER_VIEW_TYPE, PlayerView))
+#define IS_PLAYER_VIEW(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PLAYER_VIEW_TYPE))
+#define PLAYER_VIEW_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), PLAYER_VIEW_TYPE, PlayerViewClass))
+#define IS_PLAYER_VIEW_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), PLAYER_VIEW_TYPE))
+#define PLAYER_VIEW_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), PLAYER_VIEW_TYPE, PlayerViewClass))
+
+/* Private storage */
+struct _PlayerViewPrivate {
+        unsigned int x; /* Reserved */
+};
+
+/* PlayerView object */
+struct _PlayerView {
+        GtkBin parent;
+
+        PlayerViewPrivate *priv;
+};
+
+/* PlayerView class definition */
+struct _PlayerViewClass {
+        GtkBinClass parent_class;
+};
+
+
+/* Boilerplate GObject code */
+static void player_view_class_init(PlayerViewClass *klass);
+static void player_view_init(PlayerView *self);
+static void player_view_dispose(GObject *object);
+GType player_view_get_type(void);
+
+/* PlayerView methods */
+GtkWidget* player_view_new(void);
+
+#endif /* player_view_h */
