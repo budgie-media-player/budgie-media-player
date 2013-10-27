81158d2c0ba861d979b1c26665ccda89e2b7bbb6
Ikey Doherty <ikey.doherty@gmail.com>
2013-10-27 22:20:29 +0000

    Ellipsize the name column in our treeview

--- src/player-view.c
+++ src/player-view.c
@@ -59,6 +59,7 @@ static void player_view_init(PlayerView *self)
         column = gtk_tree_view_column_new_with_attributes("Name",
                 cell_text, "text", PLAYER_COLUMN_NAME);
         gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
+        g_object_set(cell_text, "ellipsize", PANGO_ELLIPSIZE_END, NULL);
         gtk_tree_view_column_set_resizable(column, TRUE);
         gtk_tree_view_column_set_sort_column_id(column, PLAYER_COLUMN_NAME);
 
