2d480ddd544e01bc86e6f0376ffced02fd2bc3cc
Ikey Doherty <ikey.doherty@gmail.com>
2013-11-22 09:15:45 +0000

    Dont bother with sign conversion.
    Merely including the gstreamer headers is enough to make the compiler
    bomb with this option.

--- common.mk
+++ common.mk
@@ -2,4 +2,4 @@ AM_CFLAGS =  -fstack-protector -Wall -pedantic \
         -Wstrict-prototypes -Wundef -fno-common \
         -Werror-implicit-function-declaration \
         -Wformat -Wformat-security -Werror=format-security \
-        -Wconversion -Werror
+        -Wno-conversion -Werror
