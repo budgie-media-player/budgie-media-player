49cc307569f7c0d122be90e4c2206e0764069c8f
Ikey Doherty <ikey.doherty@gmail.com>
2013-11-22 08:58:30 +0000

    Enable -Werror, and bail on conversion errors
    This will cause Budgie to fail to build right now.

--- common.mk
+++ common.mk
@@ -2,4 +2,4 @@ AM_CFLAGS =  -fstack-protector -Wall -pedantic \
         -Wstrict-prototypes -Wundef -fno-common \
         -Werror-implicit-function-declaration \
         -Wformat -Wformat-security -Werror=format-security \
-        -Wno-conversion
+        -Wconversion -Werror
